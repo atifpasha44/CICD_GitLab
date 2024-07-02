52fc9a7f6ebbb57c063892d4d371cc2d04690a0f
atifpasha44 <144209574+atifpasha44@users.noreply.github.com>
2024-07-02 11:33:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.56.1"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
+Provider "aws"{
+  region = "var.region"
 }
